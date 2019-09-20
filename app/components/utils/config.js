8b1c7cad655015dee267e07cfcdd3f7ec9f12d5a
Lei Zhao <leizmonk@gmail.com>
2019-09-19 22:09:03 -0400

    wip: clean up module exports

--- app/components/utils/config.js
+++ app/components/utils/config.js
@@ -1,11 +1,13 @@
-const awsConfig = {
-  region:'us-west-2',
-  accessKeyId: process.env.PREACT_APP_AWS_ACCESS_KEY_ID, 
-  secretAccessKey: process.env.PREACT_APP_AWS_SECRET_ACCESS_KEY  
-}
+const self = module.exports = { 
+  awsConfig: {
+    region:'us-west-2',
+    accessKeyId: process.env.PREACT_APP_AWS_ACCESS_KEY_ID, 
+    secretAccessKey: process.env.PREACT_APP_AWS_SECRET_ACCESS_KEY  
+  },
 
-/* TODO: need to dynamically populate key with ZIP searched to get the right file */
-const s3Params = {
-  Bucket: 'wx-aggregator',
-  Key: 'forecast_data/darksky/11435.json'
+  /* TODO: need to dynamically populate key with ZIP searched to get the right file */
+  s3Params: {
+    Bucket: 'wx-aggregator',
+    Key: 'forecast_data/darksky/11435.json'
+  }
 }
--- app/components/utils/invoke.js
+++ app/components/utils/invoke.js
@@ -3,42 +3,48 @@ const config = require('./config');
 const lambda = new AWS.Lambda(config.awsConfig);
 const s3 = new AWS.S3(config.awsConfig);
 
-onSearchSubmit(latLng, zipCode, time, darkSkyPayload) {
-  // TODO: Hook this up to API Gateway/AWS Lambda
-  console.log(latLng, zipCode, time);
-  const payload = {
-    latLng: latLng,
-    zipCode: zipCode,
-    time: time
-  };
-  var params = {
-    FunctionName: 'arn:aws:lambda:us-west-2:444167711672:function:wx-aggregator-serverless-dev-darkSkyApi', /* required */
-    ClientContext: AWS.util.base64.encode(JSON.stringify('WX-Aggregator')),
-    InvocationType: 'RequestResponse',
-    LogType: 'Tail',
-    Payload: JSON.stringify(payload) /* Strings will be Base-64 encoded on your behalf */,
-  };
-  lambda.invoke(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
+const self = module.exports = {
+  onLoad: () => {
+    console.log('loaded invoke');
+  },
 
-  let s3Path = 'forecast_data/darksky/' + zipCode + '.json';
-  let s3Params = {
-    Bucket: 'wx-aggregator',
-    Key: s3Path
+  onSearchSubmit: (latLng, zipCode, time) => {
+    // TODO: Hook this up to API Gateway/AWS Lambda
+    console.log(latLng, zipCode, time);
+    const payload = {
+      latLng: latLng,
+      zipCode: zipCode,
+      time: time
+    };
+    var params = {
+      FunctionName: 'arn:aws:lambda:us-west-2:444167711672:function:wx-aggregator-serverless-dev-darkSkyApi', /* required */
+      ClientContext: AWS.util.base64.encode(JSON.stringify('WX-Aggregator')),
+      InvocationType: 'RequestResponse',
+      LogType: 'Tail',
+      Payload: JSON.stringify(payload) /* Strings will be Base-64 encoded on your behalf */,
+    };
+    lambda.invoke(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    });
+
+    let s3Path = 'forecast_data/darksky/' + zipCode + '.json';
+    let s3Params = {
+      Bucket: 'wx-aggregator',
+      Key: s3Path
+    }
+    const getObjectPromise = s3.getObject(s3Params).promise();
+
+    getObjectPromise.then(function(data) {
+      const output = JSON.parse(data.Body);
+      darkSkyPayload = JSON.parse(output);
+
+      return darkSkyPayload;
+    }).catch(function(err) {
+      console.log(err, err.stack);
+    }).then((darkSkyPayload) => {
+      console.log('------- payload ', darkSkyPayload)
+      return darkSkyPayload
+    })
   }
-  const getObjectPromise = s3.getObject(s3Params).promise();
-
-  getObjectPromise.then(function(data) {
-    const output = JSON.parse(data.Body);
-    darkSkyPayload = JSON.parse(output);
-
-    return darkSkyPayload;
-  }).catch(function(err) {
-    console.log(err, err.stack);
-  }).then((darkSkyPayload) => {
-    console.log('------- payload ', darkSkyPayload)
-    return darkSkyPayload
-  })
 }
